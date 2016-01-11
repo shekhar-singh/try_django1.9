b22d53099c9f34724ad07301af5420c0989735e0
shekhar-singh <sschoudhary60@gmail.com>
2016-01-11 20:09:36 +0530

    new templates

--- logingf/views.py
+++ logingf/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello there .If you are reading this means you are doing good upto this.")
+    return render(request,'portal/index.html',{})
