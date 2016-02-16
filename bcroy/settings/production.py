50ac8139a6dca7eee84ce4559dffa18f0ff1ba38
shekhar-singh <sschoudhary60@gmail.com>
2016-02-16 18:59:00 +0530

    uupdate production

--- bcroy/settings/production.py
+++ bcroy/settings/production.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+import os
 from django.conf import settings
+
 DEBUG= False
 DATABASES=settings.DATABASES
 
@@ -9,7 +13,7 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-#SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
+SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
