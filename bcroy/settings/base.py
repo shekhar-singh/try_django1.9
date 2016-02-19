b71999c6a31d665bc6cf49c29555c5f42e58aec2
shekhar-singh <sschoudhary60@gmail.com>
2016-02-19 18:43:18 +0530

    added pardir for changing the default path

--- bcroy/settings/base.py
+++ bcroy/settings/base.py
@@ -160,13 +160,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
+
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
+
+
+STATIC_ROOT = os.path.join(os.path.join(PROJECT_ROOT, os.pardir), 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(os.path.join(PROJECT_ROOT, os.pardir), 'static'),
+]
 
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
